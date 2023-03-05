f88c4e19f35ba136879bd3384e4c6ebb5c4d05e6
Jacek Łudzik <jacek.ludzik@elpassion.pl>
2023-03-05 14:56:01 +0100

    update text size and padding

--- src/App.tsx
+++ src/App.tsx
@@ -4,14 +4,14 @@ import "./index.css";
 function App() {
   return (
     <div className="App">
-      <div className="flex flex-col bg-white w-full max-w-min rounded-3xl px-4 pt-4 pb-6 shadow-2xl shadow-dark-blue/20">
+      <div className="flex flex-col bg-white w-full max-w-min rounded-3xl px-4 pt-4 pb-8 shadow-2xl shadow-dark-blue/20">
         <div className="bg-white w-[300px] h-[300px] rounded-xl overflow-hidden">
           <img className='w-full h-full' src="/image-qr-code.png" alt="qr code" />
         </div>
-        <h1 className="text-dark-blue font-bold text-2xl my-4 px-2">
+        <h1 className="text-dark-blue font-bold text-2xl my-5 px-2">
           Improve your front-end skills by building projects
         </h1>
-        <p className="text-grey-blue font-normal px-3">
+        <p className="text-grey-blue font-normal px-3 text-[15px]">
           Scan the QR code to visit Frontend Mentor and take your coding skills
           to the next level
         </p>
