faa933314e4d8bb22de4e5dafba3bd1e984d4347
Jacek Łudzik <jacek.ludzik@elpassion.pl>
2023-03-05 15:17:41 +0100

    change background and image size

--- src/App.tsx
+++ src/App.tsx
@@ -5,10 +5,10 @@ function App() {
   return (
     <div className="App">
       <div className="flex flex-col bg-white w-full max-w-min rounded-3xl px-4 pt-4 pb-8 shadow-2xl shadow-dark-blue/20">
-        <div className="bg-white w-[300px] h-[300px] rounded-xl overflow-hidden">
+        <div className="bg-white w-[280px] h-[280px] rounded-xl overflow-hidden">
           <img className='w-full h-full' src="/image-qr-code.png" alt="qr code" />
         </div>
-        <h1 className="text-dark-blue font-bold text-2xl my-5 px-2">
+        <h1 className="text-dark-blue font-bold text-xl my-5 px-2">
           Improve your front-end skills by building projects
         </h1>
         <p className="text-grey-blue font-normal px-3 text-[15px]">
